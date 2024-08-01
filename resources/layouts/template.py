134a582b60a3cbff0efba20a5d6a05b9e35ebb6a
Lluis Mather <lluis@mathergroup.io>
2024-08-01 17:09:24 +0200

    typography css for loading and footer

--- resources/layouts/template.py
+++ resources/layouts/template.py
@@ -1,34 +1,6 @@
 import dash
 from dash import dcc, html
-import datetime
-
-footer = html.Div(
-    [
-        html.Div(
-            [
-                html.Div(
-                    [
-                        html.A(
-                            "{} template by Mather Group Limited".format(
-                                str(datetime.datetime.now().year)
-                            ),
-                            href="https://mathergroup.io",
-                            target="_blank",
-                            className="text-sm text-white",
-                        ),
-                        html.P(
-                            "",
-                            className="text-sm text-white",
-                        ),
-                    ],
-                    className="flex flex-col sm:flex-row sm:justify-between sm:items-center",
-                ),
-            ],
-            className="w-full flex flex-col sm:flex-row sm:justify-between sm:items-center",
-        ),
-    ],
-    className="bg-gray-800 p-8 mt-auto",
-)
+from resources.layouts.footer import footer
 
 
 def layout():
